97fceea70e072777c888ed80a9029edb1bccf673
Riya <110640624+RiyaMaurya1@users.noreply.github.com>
2023-06-01 03:12:42 +0530

    Time: 45 ms (45.20%), Space: 18.9 MB (81.33%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    // brute force
-    // time complexity : n log n + O(2n);
+    // Optimal 
+    // time complexity : n log n + O(n);
     // space complexity : O(n) 
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
@@ -10,21 +10,16 @@ public:
         
         int start = 0;
         int end = 0;
+        
         for(int i = 0 ; i < n ; i++){
             start = intervals[i][0];
             end = intervals[i][1];
-            if(!ans.empty() && end <= ans.back()[1]){
-                continue;
+            if(ans.empty() || start > ans.back()[1]){
+                ans.push_back(intervals[i]);
             }
-            for(int j = i+1 ; j < n ; j++){
-                if(intervals[j][0] <= end){
-                    end = max(end , intervals[j][1]);
-                }
-                else{
-                    break;
-                }
+            else {
+                ans.back()[1] = max(end , ans.back()[1]);
             }
-            ans.push_back({start, end});
         }
         return ans;
     }
