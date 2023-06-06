71e813755465026bbb36dd6a062211fec07d19d2
Riya <110640624+RiyaMaurya1@users.noreply.github.com>
2023-06-07 01:17:43 +0530

    Updated Solution - GfG to GitHub

--- Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -5,34 +5,83 @@ using namespace std;
 // } Driver Code Ends
 class Solution
 {
+    class Disjoint{
+        vector <int> size, parent;
+        public :
+        
+            Disjoint(int n){
+                size.resize(n+1);
+                parent.resize(n+1);
+                
+                for(int i = 0 ; i < n+1 ; i++){
+                    size[i] = 1;
+                    parent[i] = i;
+                }
+                 
+            }
+            
+            int findparent(int x){
+                if(x == parent[x]){
+                    return x;
+                }
+                return parent[x] = findparent(parent[x]);
+            }
+            
+            void makeunion(int u, int v){
+                int pu = findparent(u);
+                int pv = findparent(v);
+                
+                if(pu == pv) return ;
+                
+                if(size[pu] < size[pv]){
+                    parent[pu] = pv;
+                    size[pv] += size[pu];
+                }
+                else if(size[pu] >= size[pv]){
+                    parent[pv] = pu;
+                    size[pu] += size[pv];
+                }
+            }
+           
+    };
 	public:
-	typedef pair<int, int> pp;
+	//  Using Kruskal's ALgorithm
+	
+    // 	Time complexity : O(N+E) + O(E logE) + O(E*4α*2)
+    //                  make edges, sorting edges, applying finparent and makeunion on all edges;
+
+    //  Space Complexity :  O(N) + O(N) + O(E)
+    //                      vector size, vector parent, vector edges. 
+    
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
-        int vis[V] = {0};
-        
-        priority_queue <pp, vector<pp> , greater<pp>> pq;
-        pq.push({0,0});
-        int sum = 0;
-        while(!pq.empty()){
-            auto it = pq.top();
-            pq.pop();
-            // int parent = it.second.second;
-            int node = it.second;
-            int wt = it.first;
-            if(vis[node] == 1){
-                continue;
-            }
-            sum += wt;
-            vis[node] = 1;
-            for(auto i : adj[node]){
-                if(!vis[i[0]]){
-                    pq.push({i[1], i[0]});
-                }
+        // We need edges and than we will sort them on the basis of wt.
+        vector <pair<int,pair<int,int>>> edges;
+        for(int i = 0 ; i< V ; i++){
+            for(auto it : adj[i]){
+                int node = i;
+                int v = it[0];
+                int wt = it[1];
+                edges.push_back({wt,{node,v}});
             }
         }
+        sort(edges.begin(), edges.end());
+        int sum = 0;
+        
+        Disjoint ds(V);
+        for(auto it : edges){
+            int wt = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
+            
+            if(ds.findparent(u) != ds.findparent(v)){
+                sum += wt;
+                ds.makeunion(u,v);
+            }
+        }
+        
         return sum;
     }
 };
