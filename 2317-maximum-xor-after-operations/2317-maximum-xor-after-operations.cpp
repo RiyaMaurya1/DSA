b76c032442769d0f49425fdd0a1d31c87163e22e
Riya <110640624+RiyaMaurya1@users.noreply.github.com>
2023-06-21 17:00:37 +0530

    Time: 228 ms (5.06%), Space: 48.1 MB (25.27%) - LeetHub

--- 2317-maximum-xor-after-operations/2317-maximum-xor-after-operations.cpp
+++ 2317-maximum-xor-after-operations/2317-maximum-xor-after-operations.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int maximumXOR(vector<int>& nums) {
-        map <int,int> mp;
+        int ans = 0;
         for(int i = 0 ; i < nums.size(); i++){
             for(int j = 0 ; j < 32 ; j++){
-                if(nums[i] & 1 == 1){
-                    mp[j]++;
+                if((nums[i] & 1) == 1){
+                     ans |= (1<<j);
                 }
                 nums[i] = nums[i]>>1;
             }
         }
-        int ans = 0;
-        for(auto it : mp){
-            if(it.second > 0){
-                ans += pow(2, it.first);
-            }
-            // cout << "bit " << it.first << " no. of bits " << it.second << endl;
-        }
+       
         return ans;
+        
+        // int ans = 0;
+        // for(int i = 0 ; i < nums.size(); i++){
+        //     ans |= nums[i];
+        // }
+        // return ans;
     }
 };
