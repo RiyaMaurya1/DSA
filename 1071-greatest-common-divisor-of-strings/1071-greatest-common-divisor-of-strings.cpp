33d9f8901d07536b972dc3b714879b713dd681c7
Riya <110640624+RiyaMaurya1@users.noreply.github.com>
2023-08-30 12:52:25 +0530

    Time: 10 ms (9.63%), Space: 33.6 MB (5.40%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,13 +1,31 @@
 class Solution {
 public:
-    string gcdOfStrings(string str1, string str2) {
-        // Check if they have non-zero GCD string.
-        if (str1 + str2 != str2 + str1) {
-            return "";
+    bool valid(string str1, string str2, int k) {
+        int len1 = str1.size(), len2 = str2.size();
+        if (len1 % k > 0 || len2 % k > 0) {
+            return false;
+        } else {
+            string base = str1.substr(0, k);
+            int n1 = len1 / k, n2 = len2 / k;
+            return str1 == joinWords(base, n1) && str2 == joinWords(base, n2);
         }
-
-        // Get the GCD of the two lengths.
-        int gcdLength = gcd(str1.size(), str2.size());
-        return str1.substr(0, gcdLength);
+    }
+    string joinWords(string str, int k) {
+        string ans = "";
+        for (int i = 0; i < k; ++i) {
+            ans += str;
+        }
+        return ans;
+    }
+    
+    
+    string gcdOfStrings(string str1, string str2) {
+        int len1 = str1.length(), len2 = str2.length();
+        for (int i = min(len1, len2); i >= 1; --i) {
+            if (valid(str1, str2, i)) {
+                return str1.substr(0, i);
+            }
+        }
+        return "";
     }
 };
